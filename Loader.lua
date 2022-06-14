b30878f5e517163069540fecc299dcff7042f228
jw0902 <84353226+jw0902@users.noreply.github.com>
2022-06-15 05:07:59 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game.GameId ~= 0
-if Parvus and Parvus.Loaded then
-    Parvus.Utilities.UI:Notification({
+if Hub and Hub.Loaded then
+    Hub.Utilities.UI:Notification({
         Title = "Hub",
         Description = "Script already executed!",
         Duration = 5
@@ -8,31 +8,31 @@ if Parvus and Parvus.Loaded then
     return
 end
 
-getgenv().Parvus = {Loaded = false,Debug = false,Current = "Loader",Utilities = {}}
-Parvus.Utilities.UI = Parvus.Debug and loadfile("Hub/Utilities/UI.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Utilities/UI.lua"))()
-Parvus.Utilities.Drawing = Parvus.Debug and loadfile("Parvus/Utilities/Drawing.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Utilities/Drawing.lua"))()
-Parvus.Utilities.Misc = Parvus.Debug and loadfile("Parvus/Utilities/Misc.lua")()
-or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Utilities/Misc.lua"))()
+getgenv().Hub = {Loaded = false,Debug = false, Current = "Loader",Utilities = {}}
+Hub.Utilities.UI = Hub.Debug and loadfile("Hub/Utilities/UI.lua")()
+or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Utilities/UI.lua"))()
+Hub.Utilities.Drawing = Hub.Debug and loadfile("Hub/Utilities/Drawing.lua")()
+or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Utilities/Drawing.lua"))()
+Hub.Utilities.Misc = Hub.Debug and loadfile("Hub/Utilities/Misc.lua")()
+or loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Utilities/Misc.lua"))()
 
-Parvus.Games = {
+Hub.Games = {
     ["1054526971"] = {
         Name = "Blackhawk Rescue Mission 5",
-        Script = Parvus.Debug and readfile("Parvus/Games/BRM5.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Games/BRM5.lua")
+        Script = Hub.Debug and readfile("Hub/Games/BRM5.lua")
+        or game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Games/BRM5.lua")
     },
     ["1168263273"] = {
         Name = "Bad Business",
-        Script = Parvus.Debug and readfile("Parvus/Games/BB.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Games/BB.lua")
+        Script = Hub.Debug and readfile("Hub/Games/BB.lua")
+        or game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Games/BB.lua")
     }
 }
 
 local PlayerService = game:GetService("Players")
 local LocalPlayer = PlayerService.LocalPlayer
 local function IfGameSupported()
-    for Id, Info in pairs(Parvus.Games) do
+    for Id, Info in pairs(Hub.Games) do
         if tostring(game.GameId) == Id then
             return Info
         end
@@ -42,27 +42,27 @@ end
 LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         local QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport
-        QueueOnTeleport(Parvus.Debug and readfile("Parvus/Loader.lua")
-        or game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Loader.lua"))
+        QueueOnTeleport(Hub.Debug and readfile("Hub/Loader.lua")
+        or game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Loader.lua"))
     end
 end)
 
 local SupportedGame = IfGameSupported()
 if SupportedGame then
-    Parvus.Current = SupportedGame.Name
+    Hub.Current = SupportedGame.Name
     loadstring(SupportedGame.Script)()
-    Parvus.Utilities.UI:Notification({
-        Title = "Parvus Hub",
-        Description = Parvus.Current .. " loaded!",
+    Hub.Utilities.UI:Notification({
+        Title = "Hub",
+        Description = Hub.Current .. " loaded!",
         Duration = 5
-    }) Parvus.Loaded = true
+    }) Hub.Loaded = true
 else
-    Parvus.Current = "Universal"
-    loadstring(Parvus.Debug and readfile("Parvus/Universal.lua")
-    or game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Parvus/main/Universal.lua"))()
-    Parvus.Utilities.UI:Notification({
-        Title = "Parvus Hub",
-        Description = Parvus.Current .. " loaded!",
+    Hub.Current = "Universal"
+    loadstring(Hub.Debug and readfile("Hub/Universal.lua")
+    or game:HttpGetAsync("https://raw.githubusercontent.com/jw0902/Hub/main/Universal.lua"))()
+    Hub.Utilities.UI:Notification({
+        Title = "Hub",
+        Description = Hub.Current .. " loaded!",
         Duration = 5
-    }) Parvus.Loaded = true
+    }) Hub.Loaded = true
 end
